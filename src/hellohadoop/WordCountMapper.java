c23957db351802b671a8d6289927479db3bd3442
bugkreator <nosuchemail@nosuchdomain.com>
2015-07-06 19:49:24 +0700

    Actually working version

--- src/hellohadoop/WordCountMapper.java
+++ src/hellohadoop/WordCountMapper.java
@@ -6,18 +6,20 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import sun.misc.Regexp;
 
 
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     Text outKey = new Text("");
     IntWritable outValue = new IntWritable(1);
+    Regexp rg = new Regexp()
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
 
         String line = value.toString();
-        String[] words = line.split(" ");
+        String[] words = line.split("(\\s)+");
 
         for (String word: words)
         {
